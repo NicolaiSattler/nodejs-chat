ac9f1cc98c15ef25c72e8826cb3c762608244936
Nicolai Sattler <nicolaisattler1990@gmail.com>
2022-01-16 14:26:05 +0100

    added: nodemon dependecies added: socket.io use of rooms fixed: bootstrap styling

--- core/socket.js
+++ core/socket.js
@@ -10,22 +10,38 @@ class socket {
 
         this.server = http.createServer(app);
         this.server.listen(port, this.onListen(port));
-
         this.io = new Server(this.server);
+
+        this.init();
+    }
+
+    init() {
         this.io.on('connection', this.onConnection.bind(this));
     }
 
     onConnection(socket) {
-        console.log('a user connected');
+        console.log(`An user connected with id ${socket.id}`);
 
         socket.on('disconnect', () => {
             console.log('a user disconnected');
         });
 
-        socket.on('chat message', (msg) => {
-            this.io.emit('chat message', msg);
-            console.log(`message: ${msg}`);
+        socket.on('chat message', this.onChatMessageReceived.bind(this));
+        socket.on('join room', (room, callBack) => { 
+            socket.join(room) 
+
+            callBack(`Joined room ${room}...`);
         });
+    }
+
+    onChatMessageReceived(msg){
+        let json = JSON.parse(msg);
+
+        if (json.room && json.room != null && json.room != ''){
+            this.io.to(json.room).emit('chat message', msg);
+        }
+
+        console.log(`message: ${msg}`);
     }
 
     onListen(port) {
@@ -34,5 +50,3 @@ class socket {
 }
 
 module.exports = socket;
-
-
--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -5,6 +5,8 @@ class Chat {
         this.form = document.getElementById('form');
         this.messageInput = document.getElementById('message');
         this.userInput = document.getElementById('user');
+        this.roomInput = document.getElementById('room');
+        this.joinButton = document.getElementById('join-room');
 
         this.socket = io();
         this.init();
@@ -12,7 +14,26 @@ class Chat {
 
     init(){
         this.form.addEventListener('submit', this.onFormSubmit.bind(this));
+        this.joinButton.addEventListener('click', this.joinRoomClicked.bind(this));
+
+        this.socket.on('connect', this.onConnect.bind(this));
         this.socket.on('chat message', this.onChatMessageReceived.bind(this));
+    }
+
+    addMessage(content) {
+        const y = document.body.scollHeight;
+        const li = document.createElement('li');
+        li.setAttribute('class', 'list-group-item')
+
+        li.textContent = content;
+
+        this.messages.appendChild(li);
+
+        window.scrollTo(0, y);
+    }
+
+    joinRoomClicked(){
+        this.socket.emit('join room', this.roomInput.value, this.addMessage.bind(this));
     }
 
     onFormSubmit(e) {
@@ -22,24 +43,25 @@ class Chat {
         if (this.messageInput.value){
             let messageContent = {
                 user: this.userInput.value,
-                message: this.messageInput.value
+                message: this.messageInput.value,
+                room: this.roomInput.value
             };
             let json = JSON.stringify(messageContent);
             this.socket.emit('chat message', json);
             this.messageInput.value = '';
         }
     }
+    
+    onConnect() {
+        const content = `Connected with id ${this.socket.id}`;
+        this.addMessage(content);
+        this.roomInput.value = this.socket.id;
+    }
 
-    onChatMessageReceived(msg) {
-        let json = JSON.parse(msg);
-        let li = document.createElement('li');
-        let y = document.body.scollHeight;
+    onChatMessageReceived(message) {
+        const msg = JSON.parse(message);
+        const content = `${msg.user}: ${msg.message}`;
 
-        li.textContent = `${json.user}: ${json.message}`;
-
-        this.messages.appendChild(li);
-
-        window.scrollTo(0, y);
-
+        this.addMessage(message);
     }
 }
